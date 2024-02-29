4a3e09c78407abad5dc31fcd0ca8be98ee0c4943
OrhanSavas <orhnsavs7@gmail.com>
2024-02-29 12:32:24 +0300

    Düzenleme

---
+++ FrmDoktorBilgiDuzenle.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Proje_Hastane
+{
+    partial class FrmDoktorBilgiDuzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(131, 33);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(148, 27);
+            this.TxtAd.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 19);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Ad:";
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(131, 70);
+            this.TxtSoyad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(148, 27);
+            this.TxtSoyad.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Soyad:";
+            // 
+            // MskTC
+            // 
+            this.MskTC.Location = new System.Drawing.Point(131, 107);
+            this.MskTC.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(148, 27);
+            this.MskTC.TabIndex = 28;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 19);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "TC Kimlik No:";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(131, 142);
+            this.TxtSifre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(148, 27);
+            this.TxtSifre.TabIndex = 34;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 19);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Şifre:";
+            // 
+            // FrmDoktorBilgiDuzenle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 658);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmDoktorBilgiDuzenle";
+            this.Text = "FrmDoktorBilgiDuzenle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label3;
+    }
+}
