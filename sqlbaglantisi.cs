36040c124b9402895f4d56f64daa5937925f5d95
OrhanSavas <orhnsavs7@gmail.com>
2024-03-05 16:23:57 +0300

    SqlConnection

---
+++ sqlbaglantisi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Proje_Hastane
+{
+    internal class sqlbaglantisi
+    {
+        public SqlConnection baglanti() 
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=Dante\\SQLEXPRESS;Initial Catalog=HastaneProje;Integrated Security=True;Trust Server Certificate=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
